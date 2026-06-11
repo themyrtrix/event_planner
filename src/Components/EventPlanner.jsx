10d002e35a079f1dfc964011f3bcd24cabfc0fe3
myrkintosh <myrtlejohnm@gmail.com>
2026-06-11 15:12:57 +0800

    Update package.json to upgrade Vite version and enhance EventPlanner component with additional sections and content

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,14 +1,55 @@
-import React from 'react';
-import './EventPlanner.css'; // Import CSS file for styling
+import React from "react";
+import "./EventPlanner.css"; // Import CSS file for styling
 
 const EventPlanner = () => {
-    return (
-        <div className="event-planner-container">
-            <header>
-                <h1>Welcome to Event Planner</h1>
-            </header>
-        </div>
-    );
+  return (
+    <div className="event-planner-container">
+      <header>
+        <h1>Welcome to Event Planner</h1>
+      </header>
+      <section className="description">
+        <p>
+          Plan and organize your events effortlessly with Event PLanner. From
+          birthdays to corporate meetings, we've got you covered.
+        </p>
+        <button className="get-started-button">Get Started</button>
+      </section>
+      <section className="events_categories">
+        <ul>
+            <h2>Event Categories</h2>
+            <li>Birthday parties</li>
+            <li>Anniversary celebrations</li>
+            <li>Wedding receptions</li>
+            <li>Baby showers</li>
+            <li>Graduation parties</li>
+            <li>Family reunions</li>
+        </ul>
+        <ul>
+            <h2>Entertainment Events</h2>
+            <li>Concerts</li>
+            <li>Music festivals</li>
+            <li>Film screenings</li>
+            <li>Comedy shows</li>
+            <li>Art exhibitions</li>
+            <li>Cultural events</li>
+        </ul>
+        <ul>
+            <h2>Community Events</h2>
+            <li>Fundraising events</li>
+            <li>Charity galas</li>
+            <li>Volunteer drives</li>
+            <li>Neighborhood block parties</li>
+            <li>Community festivals</li>
+            <li>Cultural celebrations</li>
+        </ul>
+      </section>
+      <section className="features">
+        
+      </section>
+      <section className="testimonials"></section>
+      <section className="contact"></section>
+    </div>
+  );
 };
 
 export default EventPlanner;
